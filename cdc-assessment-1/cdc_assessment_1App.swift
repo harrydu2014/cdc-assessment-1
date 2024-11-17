1cd7010ebe8f22a769baf7eb79734a57d23c2e54
Harry Du <dumingyuan0829@163.com>
2024-11-17 12:53:41 +0800

    Initial Commit

---
+++ cdc-assessment-1/cdc_assessment_1App.swift
@@ -0,0 +1,17 @@
+//
+//  cdc_assessment_1App.swift
+//  cdc-assessment-1
+//
+//  Created by Harry, Du on 2024/11/17.
+//
+
+import SwiftUI
+
+@main
+struct cdc_assessment_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
